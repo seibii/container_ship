b43d81a4ae7a57d5f0f561c7209b8c5adb72325e
Yuji Ueki <unhappychoice@gmail.com>
2019-10-24 19:00:26 +0900

    :green_heart: Pass test

--- spec/container_ship_spec.rb
+++ spec/container_ship_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ContainerShip do
   it 'has a version number' do
     expect(ContainerShip::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
