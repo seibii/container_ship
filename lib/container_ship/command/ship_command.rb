ec8849d9468d2b597552720dab00d0295ec7330b
Yuji Ueki <unhappychoice@gmail.com>
2019-10-24 13:37:51 +0900

    :recycle: Refactor ship command

--- lib/container_ship/command/ship_command.rb
+++ lib/container_ship/command/ship_command.rb
@@ -2,61 +2,34 @@ require 'aws-sdk-ecs'
 require 'json'
 require 'open3'
 
+require 'container_ship/command/modules/docker'
+require 'container_ship/command/modules/ecs'
+
 module ContainerShip
   module Command
     class ShipCommand
+      include Modules::Docker
+      include Modules::Ecs
+      include Moudles::PrintTask
+
       def run(cluster_name, service_name, environment, build_number)
-        json = read_task_definition(cluster_name, service_name, environment)
-
-        definition = json[:container_definitions]
-          .find { |definition| definition[:image].include?('<image_number>') }
-
-        image_name = definition[:image].split(':').first
-        definition[:image] = definition[:image].gsub('<image_number>', build_number.to_s)
-
-        push_image(image_name, build_number)
-        revision = register(json)
-        update_service(cluster_name, service_name, environment, revision)
-      end
-
-      private
-
-      def aws_ecs_client
-        @aws_ecs_client ||= Aws::ECS::Client.new
-      end
-
-      def sh(command)
-        Open3.popen3(command) { |_i, o, _e, _w| o.each { |line| puts line } }
-      end
-
-      def read_task_definition(cluster_name, service_name, environment)
-        file = File.read(File.join('.container_ship', cluster_name, 'services', service_name, environment, "task_definition.json"))
-        JSON.parse(file, symbolize_names: true)
-      end
-
-      def push_image(image_name, build_number)
-        sh "docker build -t \"#{image_name}:#{build_number}\" ."
-        sh "docker push #{image_name}:#{build_number}"
-      end
-
-      def register(json)
-        print "Registering task definition... "
-        revision = aws_ecs_client
-          .register_task_definition(json)
-          .task_definition
-          .revision
-        puts "Done"
-        revision
-      end
-
-      def update_service(cluster_name, service_name, environment, revision)
-        print "Updating service... "
-        aws_ecs_client.update_service(
-          cluster: "#{cluster_name}-#{environment}",
-          service: "#{cluster_name}-#{service_name}-#{environment}",
-          task_definition: "#{cluster_name}-#{environment}:#{revision}"
+        task_definition = TaskDefinition.new(
+          cluster_name,
+          'services',
+          service_name,
+          environment,
+          build_number
         )
-        puts 'Done'
+
+        push_image task_definition
+
+        revision = print_around_task('Registering task definition... ') do
+          register task_definition
+        end
+
+        print_around_task('Updating service... ') do
+          update_service task_definition, revision
+        end
       end
     end
   end
